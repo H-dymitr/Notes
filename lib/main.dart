71350c9b9c43ef9bbfda2b901bbcad045d6cde16
ip.web.so <ip.web.so@gmail.com>
2024-05-05 13:07:45 +0000

    style: update theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 import 'screens/note_list_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Notes App',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.dark,
+        ),
+        textTheme: TextTheme(
+        displayLarge: const TextStyle(
+          fontSize: 72,
+          fontWeight: FontWeight.bold,
+        ),
+        // ···
+        titleLarge: GoogleFonts.oswald(
+          fontSize: 30,
+          fontStyle: FontStyle.italic,
+        ),
+        bodyMedium: GoogleFonts.merriweather(),
+        displaySmall: GoogleFonts.pacifico(),
       ),
-      home: NoteListScreen(),
+      ),
+      home: const NoteListScreen(),
     );
   }
 }
